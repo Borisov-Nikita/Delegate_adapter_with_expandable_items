a0b43f4542d8e7ecfd10684f4e83e3b00d60f113
Borisov Nikita <borisov-ta@yandex.ru>
2023-11-09 08:58:02 +0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Delegate adapter with expandable items"
+include(":app")
+ 
